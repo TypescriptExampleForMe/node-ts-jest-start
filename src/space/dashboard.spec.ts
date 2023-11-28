0bd11e01d8d35df6362d987401d0cf5a08a89c18
Yariv Gilad <yariv.gilad@gmail.com>
2023-11-28 10:35:12 +0200

    initial commit

---
+++ src/space/dashboard.spec.ts
@@ -0,0 +1,12 @@
+import { Alien, lifeForms} from './dashboard';
+
+describe('tests for Alien class',()=>{
+    test('greet returns correct value',()=> {
+        // Arrange
+        const alf = new Alien(100) ;
+        // Act
+        const expected = alf.greet(lifeForms.martian);
+        // Assert
+        expect(expected).toBe(`Greeting a Martian`);
+    })
+})
---
+++ src/space/dashboard.ts
@@ -0,0 +1,23 @@
+let shipCount = 3;
+
+export function countShips(){
+    return shipCount * 3; 
+}
+
+export enum lifeForms{
+    human='Human', 
+    martian='Martian', 
+    reptilian='Reptilian', 
+    angel='Angel', 
+    borg='Borg'
+}
+
+export class Alien{
+    constructor(private _age:number){}
+
+    greet(lifeForm:lifeForms){
+        return `Greeting a ${lifeForm}`
+    }
+    
+}
+
